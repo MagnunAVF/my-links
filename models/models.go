a5cce34ea60ee3ff84da6221d96f82dc41da2abc
Magnun A V F <magnunavf@gmail.com>
2019-04-16 02:43:44 -0300

    Fix and Improve User model

--- models/models.go
+++ models/models.go
@@ -1,12 +1,15 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-	Name  string `json:"name,omitempty"`
-	Email string `json:"email,omitempty"`
-	Link  `json:"link,omitempty"`
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name  string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email string             `json:"email,omitempty" bson:"email,omitempty"`
+	Links []Link             `json:"links,omitempty" bson:"links,omitempty"`
 }
 
 type Link struct {
-	Title string `json:"title,omitempty"`
-	Url   string `json:"url,omitempty"`
+	Title string `json:"title,omitempty bson:"title,omitempty""`
+	Url   string `json:"url,omitempty bson:"_url,omitempty""`
 }
